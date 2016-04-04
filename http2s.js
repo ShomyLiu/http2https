31954d6fe712fd7adbe776f51abb615542ed733b
ShomyLiu <18222333186@163.com>
2016-04-04 17:22:40 +0800

    update

---
+++ http2s.js
@@ -0,0 +1,13 @@
+// store the preUrl in case of circle,repeated
+var urlHistoryMap = [];
+
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+  if(!/http:\/\/.*?google.com/.test(tab.url)) return;
+	if(info.status == "loading") {
+    var preUrl = urlHistoryMap[tabId];
+    if(null == preUrl || preUrl != tab.url){
+	    chrome.tabs.update(tab.id, {url: tab.url.replace("http://", "https://"),highlighted:true});
+      urlHistoryMap[tabId] = tab.url;
+    }
+  }
+});
